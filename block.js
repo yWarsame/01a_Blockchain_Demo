a90693ea7a64fab372542005edb5ada7396e1d48
Yasin Warsame <yasinhassan1013@gmail.com>
2026-03-24 10:55:49 +0000

    SHA256()

--- block.js
+++ block.js
@@ -1,32 +1,43 @@
 // Klasse
-class Block {
 
-    constructor (timestamp,lastHash, hash, data){
-        this.timestamp=timestamp; // zeitstempel
-        this.lastHash=lastHash; // Hash des Vorhergehenden Blocks
-        this.hash=hash; // Eigener Hash
-        this.data=data; // Daten
+const SHA256 = require("crypto-js/sha256");
+
+class Block{
+
+    constructor(timestamp, lastHash, hash, data){
+        this.timestamp = timestamp; // Zeitstempel
+        this.lastHash = lastHash; // Hash d. Vorgängers
+        this.hash = hash; //Eigener Hash
+        this.data = data; // Eigene Blockdaten
     }
-    // ausgabe funktion
-    toString()
-    { // für Debugging-Zwecke
+
+    toString(){ // für Debugging-Zwecke
         return `Block -
         Timestamp:  ${this.timestamp}
         Last Hash:  ${this.lastHash}
         Hash:       ${this.hash}
         Data:       ${this.data}`
+    }
+
+    static genesis(){ // 1. Block der chain
+        // call constructor
+        return new this("GenesisTime","000000","#abcdC1234","GenesisData"); 
+    }
+
+    static mineBlock(lastBlock, blockData){ // weitere Blocks in der Chain
+        
+        const timestamp = Date.now(); // Zeit im ms seit 01.01.1970 | UNIX
+        const lastHash  = lastBlock.hash; // HASH-Wert des Vorgängers
+        // const hash      = "Hash to do: data";  // Fake
+        const hash      = Block.hash(timestamp,lastHash,blockData); // SHA256
+
+        return new this(timestamp, lastHash, hash, blockData); // call constructor
 
     }
 
-    static genesis()
-    { // 1. Block der chain
-        return new this("GenesisTime","000000","#abcdC1234","GenesisData"); // call constructor
+    static hash(timestamp,lastHash,data){
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
     }
-
-
-
-
-
 
 
 
@@ -35,4 +46,5 @@ class Block {
 
 
 }
+
 module.exports = Block; // Export als Modul
--- dev-test.js
+++ dev-test.js
@@ -1,7 +1,13 @@
 // call: npm run dev-test
+
 const Block = require("./block");
+
 /*  Test der Block-Funktionalität  */
 
- const block = new Block("aktuelle zeit","hash1","hash2","meine Daten");
- console.log(block.toString());
+const block = new Block("aktuelle zeit","hash1","hash2","meine Daten");
+console.log(block.toString());
 console.log(Block.genesis().toString());
+
+
+const testBlock = Block.mineBlock(Block.genesis(),"testDaten")
+console.log(testBlock.toString());
